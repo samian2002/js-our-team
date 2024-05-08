d52693cd77444c071040791a3f361add4966aecc
samian2002 <samiantariq14@gmail.com>
2024-05-08 10:46:03 +0200

    m1- definisco gli oggetti nell'array

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const team = [
+    { nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "../img/wayne-barnett-founder-ceo.jpg" },
+    { nome: "Angela Caroll", ruolo: "Chief Editor", foto: "../img/angela-caroll-chief-editor.jpg" },
+    { nome: "Walter Gordon", ruolo: "Office Manager", foto: "../img/walter-gordon-office-manager.jpg" },
+    { nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "../img/angela-lopez-social-media-manager.jpg" },
+    { nome: "Scott Estrada", ruolo: "Developer", foto: "../img/scott-estrada-developer.jpg" },
+    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "../img/barbara-ramos-graphic-designer.jpg" }
+  ];
+  
